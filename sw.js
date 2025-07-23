215242b4721ba418c81b1d39b16cc9f76bc40719
htwoop <anastasiyanemchinova@gmail.com>
2025-07-23 11:18:11 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,17 +10,28 @@ const urlsToCache = ['/',
     '/paint_image3.jpg'];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) =>
+            cache.addAll(urlsToCache))
+    );
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', function (event) {
+    event.respondWith(fromCache(event.request));
+    event.waitUntil(update(event.request));
 });
+
+function fromCache(request) {
+    return caches.open(CACHE_NAME).then((cache) =>
+        cache.match(request).then((matching) =>
+            matching || Promise.reject('no-match')
+        ));
+}
+
+function update(request) {
+    return caches.open(CACHE_NAME).then((cache) =>
+        fetch(request).then((response) =>
+            cache.put(request, response)
+        )
+    );
+}
